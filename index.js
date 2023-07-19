c7f8e743429bdbedac21172780e9f2efd1df10dc
giovgio90 <giovgio90@gmail.com>
2023-07-19 11:33:15 +0200

    graphic update

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ let seconds = document.getElementById("seconds");
 let ss = document.getElementById("ss");
 let secDot = document.querySelector(".dots-sec_dot");
 
-let countDown = 59; // Imposta il valore di countdown a 60 secondi
+let countDown = 60; // Imposta il valore di countdown a 60 secondi
 let s = countDown; // Imposta il valore iniziale dei secondi al valore di countdown
 
-seconds.innerHTML = s; // Mostra il valore iniziale dei secondi
+seconds.innerText = s; // Mostra il valore iniziale dei secondi
 
 let x = setInterval(function () {
   s--;
 
-  seconds.innerHTML = s; // Aggiorna il valore dei secondi
+  seconds.innerText = s; // Aggiorna il valore dei secondi
 
-  ss.style.strokeDashoffset = (440 * s) / countDown;
+  ss.style.strokeDashoffset = 385 + (385 * s) / countDown;
   ss.style.stroke = "#00FFFF";
 
   if (s === 0) {
